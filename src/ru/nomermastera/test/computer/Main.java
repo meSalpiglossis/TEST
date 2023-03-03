48eb0e65e63deac07979c8e7abdb41b65c965984
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-03-03 21:05:22 +1000

    short record

--- src/ru/nomermastera/test/computer/Main.java
+++ src/ru/nomermastera/test/computer/Main.java
@@ -2,8 +2,7 @@ package ru.nomermastera.test.computer;
 
 public class Main {
     public static void main(String[] args) {
-        Monitor monitor = new Monitor("Samsung");
-        Computer computer = new Computer("MacPro", monitor);
+        Computer computer = new Computer("MacPro", new Monitor("Samsung"));
 
         computer.On();
         computer.Off();
