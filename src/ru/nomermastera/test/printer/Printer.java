bbcc22f73b941f68b5917bd5dd982ca6b3e34985
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-03-05 03:28:52 +1000

    Printer.java

---
+++ src/ru/nomermastera/test/printer/Printer.java
@@ -0,0 +1,64 @@
+package ru.nomermastera.test.printer;
+
+public class Printer {
+
+    private int paintVolume;
+
+    public Printer() {
+        this.paintVolume = 0;
+    }
+
+    public Printer(int paintVolume) {
+        this.paintVolume = paintVolume;
+    }
+
+    public int getPaintVolume() {
+        return paintVolume;
+    }
+
+    public void setPaintVolume(int paintVolume) {
+        this.paintVolume = paintVolume;
+    }
+
+    public void charge(int addPaintVolume) {
+        this.paintVolume = this.paintVolume + addPaintVolume;
+        System.out.println("Принтер пополнен на длину текста " + addPaintVolume + ". Сейчас краски в принтере: " + this.paintVolume);
+    }
+
+    public void print(String inString) {
+        int lettersAmount = lettersAmount(inString);
+        int inkDelta = this.paintVolume - lettersAmount;
+        System.out.println("На печать отправлен текст \"" + inString + "\", длина: " + lettersAmount);
+        if (inkDelta < 0) {
+            System.out.println("Не хватило краски в принтере: " + Math.abs(inkDelta) + ". Сейчас краски в принтере: " + this.paintVolume);
+        } else {
+            this.paintVolume = inkDelta;
+            System.out.println("Текст распечатан. Осталось краски: " + this.paintVolume);
+        }
+    }
+
+    private int lettersAmount(String string) {
+        int lettersAmount = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ') {
+                lettersAmount++;
+            }
+        }
+        return lettersAmount;
+    }
+
+    public static void main(String[] args) {
+
+        Printer printer = new Printer(1);
+
+        printer.print("123456789");
+
+        printer.charge(8);
+
+        printer.print("123456789");
+
+        printer.charge(8);
+
+    }
+
+}
